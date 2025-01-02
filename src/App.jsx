6608664f35798abf98bc8fa13859f2139af14487
Marcelo Carvalho <marceloodev@gmail.com>
2025-01-01 21:36:49 -0300

    Adicionado backgroud e margens no formulário

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   max-width: 800px;
-  margin: 20px auto;
+  margin: 200px auto;
   background: #fff;
   border-radius: 8px;
   padding: 20px;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
 
-import App from './App';
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #000000;
+    margin: 0;
+    padding: 0;
+    font-family: Arial, sans-serif;
+  }
+`;
 
-createRoot(document.getElementById('root')).render(
+import App from "./App";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <GlobalStyle />
+
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
